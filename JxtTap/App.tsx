3831b6f73b68bb72bf5d8504bd00dc3fb099aa1d
Nachi <nachishauli@gmail.com>
2026-06-27 20:28:02 +0200

    feat: add React Native JxtTap app

---
+++ JxtTap/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5F5FF' }}>
+      <Text style={{ fontSize: 24, color: '#2D3FCC' }}>Jxt Tap is alive! 🎉</Text>
+    </View>
+  );
+}
+
+export default App;
